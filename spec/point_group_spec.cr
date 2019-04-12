3f5e44d92950bb4f3f8a55215f4b63903f611682
KC Erb <iamkcerb@gmail.com>
2019-04-12 13:45:22 +0200

    add inverse and product methods

--- spec/point_group_spec.cr
+++ spec/point_group_spec.cr
@@ -3,7 +3,7 @@ require "./spec_helper"
 module Symm32
   describe PointGroup do
     context "point group 2/m" do
-      pg2m = PointGroup.parse("monoclinic", "2/m", ["e", "2z", "i", "mz"])
+      pg2m = Symm32.point_group("2/m")
 
       it "has basic properties" do
         pg2m.family.should eq Family::Monoclinic
@@ -44,6 +44,69 @@ module Symm32
         pg2m.plane.empty?.should be_true
         pg2m.diags.empty?.should be_true
         pg2m.edges.empty?.should be_true
+      end
+    end
+
+    # we'll check inverse and multiplication on a group where some
+    # isometries are not their own inverse
+    context "group operators" do
+      pg1 = Symm32.point_group("1")
+      pg4 = Symm32.point_group("4")
+      identity = pg1.isometries.first
+      pg4_isometries = pg4.isometries.to_a
+      rot4 = pg4_isometries[1]
+      rot2 = pg4_isometries[2]
+      rot4_3 = pg4_isometries.last
+
+      it "identity is own inverse" do
+        pg1.inverse(identity).should eq identity
+      end
+
+      it "can handle isometry from other group" do
+        pg4.inverse(identity).should eq identity
+      end
+
+      it "inverse of 4 is 4^3" do
+        pg4.inverse(rot4).should eq rot4_3
+      end
+
+      it "inverse of 2 is 2 though" do
+        pg4.inverse(rot2).should eq rot2
+      end
+
+      it "identity times anything is itself" do
+        pg1.product(identity, identity).should eq identity
+        pg4.product(identity, rot4_3).should eq rot4_3
+        pg4.product(rot2, identity).should eq rot2
+      end
+
+      it "some other products" do
+        pg4.product(rot4, rot4).should eq rot2
+        pg4.product(rot4, rot4_3).should eq identity
+        pg4.product(rot2, rot4).should eq rot4_3
+      end
+
+      # 32 is smallest non-abelian point group
+      # note that several conventions come together to make the following result:
+      # left action convention => AB means transform with B and then A
+      # active rotations => rotate points, not coordinates
+      # counter-clockwise is positive => rot3 => rotate point counter-clockwise 1/3 of the way
+      context "non-abelian point group 32" do
+        pg32 = Symm32.point_group("32")
+        pg32_isometries = pg32.isometries.to_a
+        # ["e", "3z", "3z^2", "2t30", "2t90", "2t150"]
+        rot3 = pg32_isometries[1]
+        rot2t30 = pg32_isometries[3]
+        rot2t90 = pg32_isometries[4]
+        rot2t150 = pg32_isometries[5]
+
+        it "rot3 * rot2t30 = 2t90" do
+          pg32.product(rot3, rot2t30).should eq rot2t90
+        end
+
+        it "rot2t30 * rot3 = 2t150" do
+          pg32.product(rot2t30, rot3).should eq rot2t150
+        end
       end
     end
   end
--- src/point_group.cr
+++ src/point_group.cr
@@ -13,10 +13,18 @@ module Symm32
     getter family : Family
     getter directions : Array(Direction)
 
+    private alias IsoHash = Hash(SymmBase::Isometry, SymmBase::Isometry)
+    @inverses_hash = IsoHash.new
+    # nested hash that acts like a 2D nested table where we index by isometry rather than number
+    private alias Hash2DIsometry = Hash(SymmBase::Isometry, IsoHash)
+    @multiplication_table = Hash2DIsometry.new
+
     def initialize(@family, @name, isometries_arr)
       super(@name, isometries_arr)
       @directions = init_directions
       @family.classify_directions(@directions)
+      init_inverses
+      init_multiplication_table
     end
 
     # Creates a point group from "minimal" strings.
@@ -29,7 +37,16 @@ module Symm32
       new(Family.parse(family), name, isometries)
     end
 
-    def inverse?
+    # left action convention means product(iso1, iso2) => apply iso2 and then apply iso1
+    def product(iso1, iso2)
+      @multiplication_table[iso1][iso2]
+    end
+
+    def inverse(iso)
+      @inverses_hash[iso]
+    end
+
+    def centrosymmetric?
       isometries.map(&.kind).includes? :inversion
     end
 
@@ -88,6 +105,73 @@ module Symm32
         Direction.new(axis, iso_arr)
       end
       dirs.sort_by(&.axis)
+    end
+
+    private def init_inverses
+      isometries.each do |iso|
+        rot_hash = parse_rotation(iso)
+        next @inverses_hash[iso] = iso unless rot_hash
+        next @inverses_hash[iso] = iso if rot_hash[:n_fold] == 2
+        inv_kind = "#{rot_hash[:base]}#{rot_hash[:n_fold]}"
+        if rot_hash[:power] == 1
+          power = rot_hash[:n_fold] - 1
+          inv_kind += "_#{power.to_s}"
+        end
+
+        iso_inv = isometries.find do |i|
+          res = i.kind.to_s == inv_kind
+          if iso.responds_to?(:axis) && i.responds_to?(:axis)
+            res &= i.axis == iso.axis
+          end
+          res
+        end.not_nil!
+        @inverses_hash[iso] = iso_inv
+      end
+    end
+
+    # MULTIPLICATION TABLE
+    # -  This is in regular representation order, so the columns are in any order
+    #    starting with identity
+    # -  Rotations are active and use the standard x->y counter-clockwise convention.
+    #    Therefore, the product of 3 * 2_T30 for example in PG 32 is 2_T90 instead of
+    #    2_T150 which we'd get from a clockwise rotation or passive rotation as seems
+    #    to be common in the chemistry references I've seen.
+    private POLAR         = Math::PI/2 - Math::PI/23
+    private AZIMUTHAL     = Math::PI/2 - Math::PI/17
+    private COORDS        = SymmBase::Vector3.new(POLAR, AZIMUTHAL)
+    GENERAL_POINT = ChiralPoint.new(COORDS)
+
+    private def init_multiplication_table
+      # note that the following means subclasses just need to define GENERAL_POINT
+      # and then the rest comes from here:
+      general_point = {% begin %}{{ @type }}::GENERAL_POINT{% end %}
+      # turn isometries into points for multiplication by transformation
+      mult_map = Hash(SymmBase::Isometry, SymmBase::Point).new
+      isometries.each do |iso|
+        mult_map[iso] = iso.transform(general_point)
+      end
+
+      # fill the table with results from the map
+      size = isometries.size
+      @multiplication_table = Hash2DIsometry.new
+      isometries.each do |inv_row_iso|
+        row_iso = @inverses_hash[inv_row_iso]
+        @multiplication_table[row_iso] = IsoHash.new
+        isometries.each do |col_iso|
+          point = row_iso.transform(col_iso.transform(general_point))
+          @multiplication_table[row_iso][col_iso] = mult_map.key_for(point)
+        end
+      end
+    end
+
+    # turns a rotation kind symbol into a useful hash
+    private def parse_rotation(iso)
+      match = iso.kind.to_s.match(/(.*rotation_)(\d.*)/)
+      return match unless match
+      suffix = match[2]
+      n_fold = suffix[0].to_i
+      power = suffix.size > 1 ? suffix[2].to_i : 1
+      { base: match[1], n_fold: n_fold, power: power}
     end
   end
 end
